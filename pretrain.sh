b9168b78faef363311895be79311a6e37d64bd7d
unknown <brightgems@live.cn>
2019-12-17 23:15:00 +0800

    long warmup

--- pretrain.sh
+++ pretrain.sh
@@ -2,7 +2,6 @@ sh dataset/prepare_data.sh /data/corpus/ ./train_512/ 16
 
 python3 train.py -config_file configs/base.json \
 -input_file train_512/* -output_dir models_512 \
--init_checkpoint gs://pyai/models/mega/model.ckpt-100000 \
--max_seq_length 1024 -train_batch_size 16 -lea-rning_rate 1e-6 \
--num_train_steps 100000 -num_warmup_steps 10000 -save_checkpoints_steps 1000 \
--iterations_per_loop 1000 -use_tpu 1 -tpu_name brightgems
+-max_seq_length 512 -train_batch_size 4 -learning_rate 3e-6 \
+-num_warmup_steps 50000 -num_train_steps 100000\
+-save_checkpoints_steps 10000
